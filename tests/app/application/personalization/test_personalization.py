d1ca20fe7605eba0f0d3fbffbf829ebc8bf62215
Hongmin <nhm9001@gmail.com>
2025-01-20 17:47:01 +0900

    add pytest - personalization

--- tests/app/application/personalization/test_personalization.py
+++ tests/app/application/personalization/test_personalization.py
@@ -23,7 +23,7 @@ BASE_URL = "http://test"
 
 
 @pytest.mark.asyncio
-async def test_create_user_feature_http(session: AsyncSession):
+async def test_personalization_create_user_http(session: AsyncSession):
 
     # Given
     user = make_user(**users[1])
@@ -36,11 +36,14 @@ async def test_create_user_feature_http(session: AsyncSession):
     # await session.commit()
 
     user_id = user_feature.user_id
+    protocol = "http"
+    size = 2048
+    dtype = "float16"
     body = {
         "user_id": user_id,
-        "protocol": "http",
-        "size": 2048,
-        "dtype": "float16",
+        "protocol": protocol,
+        "size": size,
+        "dtype": dtype,
     }
 
     # When
@@ -53,3 +56,94 @@ async def test_create_user_feature_http(session: AsyncSession):
 
     # Then
     sut = response.json()
+
+    assert str(sut["user_id"]) == str(user_id)
+    assert isinstance(sut["user_vector"], list)
+    assert len(sut["user_vector"][0]) == size
+
+
+@pytest.mark.asyncio
+async def test_personalization_update_user_http(session: AsyncSession):
+
+    # Given
+    user = make_user(**users[1])
+    user_feature = make_user_feature(**user_features[1])
+    np_vector = np.expand_dims(
+        np.frombuffer(user_feature.bvector, dtype=getattr(np, user_feature.dtype)),
+        axis=0,
+    )
+    user_vector = np_vector.astype(np.float64).tolist()
+
+    # Given
+    session.add(user)
+    await session.commit()
+    session.add(user_feature)
+    await session.commit()
+
+    user_id = user_feature.user_id
+    protocol = "http"
+    size = 2048
+    dtype = "float16"
+    body = {
+        "user_id": user_id,
+        "protocol": protocol,
+        "size": size,
+        "dtype": dtype,
+    }
+
+    # When
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as client:
+        response = await client.post(
+            f"/api/v1/personalization/user/{user_id}/update", headers=HEADERS, json=body
+        )
+
+    # Then
+    sut = response.json()
+
+    assert str(sut["user_id"]) == str(user_id)
+    assert isinstance(sut["user_vector"], list)
+    assert len(sut["user_vector"][0]) == size
+    assert sut["user_vector"] != user_vector
+
+
+@pytest.mark.asyncio
+async def test_personalization_get_user_http(session: AsyncSession):
+
+    # Given
+    user = make_user(**users[1])
+    user_feature = make_user_feature(**user_features[1])
+
+    # Given
+    session.add(user)
+    await session.commit()
+    session.add(user_feature)
+    await session.commit()
+
+    user_id = user_feature.user_id
+    protocol = "http"
+    size = 2048
+    dtype = "float16"
+
+    # When
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as client:
+        response = await client.get(
+            f"/api/v1/personalization/user/{user_id}",
+            headers=HEADERS,
+            params={"user_id": user_id},
+        )
+
+    # Then
+    sut = response.json()
+
+    np_vector = np.expand_dims(
+        np.frombuffer(user_feature.bvector, dtype=getattr(np, user_feature.dtype)),
+        axis=0,
+    )
+    user_vector = np_vector.astype(np.float64).tolist()
+
+    assert str(sut["user_id"]) == str(user_id)
+    assert sut["user_vector"] == user_vector
